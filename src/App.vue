9d12b18da3d394e59bf44c4d76e897b6415bd5fe
杨坤海 <384037191@qq.com>
2022-07-29 11:08:57 +0800

    add router

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+ 
+</script>
+
+
+<template>
+  <div>
+    <router-link to="/">首页</router-link> | 
+    <router-link to="/about">关于</router-link>
+  </div>
+  <router-view></router-view>
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
